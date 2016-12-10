0562ef6da59f7dd0b213ee5b9679ff08c6946da5
Goto01 <kirill.tkachev.ktkach@gmail.com>
2016-12-10 18:39:37 +0300

    * Pixel perfect

--- Project/LD37/Assets/Scripts/Core/Depended/MouseRotatedComponent.cs
+++ Project/LD37/Assets/Scripts/Core/Depended/MouseRotatedComponent.cs
@@ -4,10 +4,12 @@ namespace Assets.Scripts.Core.Depended
 {
     class MouseRotatedComponent : CustomMonoBehaviour
     {
+        [SerializeField] private Camera _camera;
+
         protected virtual void Update()
         {
-            var way = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2) transform.position;
-            Debug.DrawLine((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.one);
+            var way = (Vector2)_camera.ScreenToWorldPoint(Input.mousePosition) - (Vector2) transform.position;
+            Debug.DrawLine((Vector2)_camera.ScreenToWorldPoint(Input.mousePosition), Vector3.one);
             float angle = Mathf.Atan2(way.y, way.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
